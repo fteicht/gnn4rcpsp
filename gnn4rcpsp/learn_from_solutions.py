904c3bc3cab26b1e04eb6df13f50f8e2de2c8412
Florent Teichteil-Koenigsbuch <florent.teichteil-koenigsbuch@airbus.com>
2022-03-17 22:45:20 +0100

    More refined statistics in learn_from_solutions.py

--- gnn4rcpsp/learn_from_solutions.py
+++ gnn4rcpsp/learn_from_solutions.py
@@ -1,3 +1,4 @@
+import random
 import torch
 import torch.nn.functional as F
 from torch.utils.tensorboard import SummaryWriter
@@ -13,15 +14,29 @@ from graph import load_data
 from models import ResTransformer, ResGINE
 
 
+def compute_loss(data, device):
+    data_batch = data
+    # Iterate over batch elements for simplicity
+    # TODO: vectorize?
+    batch_loss = torch.tensor(0., dtype=torch.float32).to(device)
+    data_list = data_batch.to_data_list()
+    for data in data_list:
+        batch_loss += F.mse_loss(data.out[len(data.rc):,0], data.solution_starts)
+    return batch_loss / len(data_list)
+
+
 def check_solution(data):
     data_batch = data
     # Iterate over batch elements for simplicity
     # TODO: vectorize?
     total_violations = {
-            'all_positive': [],
-            'precedence': [],
+            'all_positive_per': [],
+            'all_positive_mag': [],
+            'precedence_per': [],
+            'precedence_mag': [],
+            'resource_per': [],
+            'resource_mag': [],
             'makespan': [],
-            'resource': [],
         }
     for data in data_batch.to_data_list():
         
@@ -35,33 +50,50 @@ def check_solution(data):
         )
         nb_tasks = len(dur)
         nb_res = len(rc)
-        s = data.out.data.cpu().detach().numpy()[nb_res:]
+        s = data.out.data.cpu().detach().numpy()[nb_res:].flatten()
         s = s - s.min()
     
         # All starting times must be positive
-        all_positive_violations = np.sum((s < 0.0).astype(int)) / nb_tasks * 100.0
+        negative_s = (s < 0.0).astype(int)
+        all_positive_violations = np.sum(negative_s) / nb_tasks * 100.0
+        all_positive_magnitude = np.sum(np.dot(s, negative_s)) / len(negative_s) if len(negative_s) > 0 else 0
 
         # Precedence constraints
-        precedence_violations = np.sum([np.sum((np.array([s[p] + t2t[t][p] - s[t]
-                                                          for p in range(t2t.shape[1]) if t2t[t][p] > 0]) > 0).astype(int))
+        pred_viol_idx = [(np.array([s[p] + t2t[t][p] - s[t]
+                                    for p in range(t2t.shape[1]) if t2t[t][p] > 0]) > 0).astype(int)
+                         for t in range(t2t.shape[0])]
+        precedence_violations = np.sum([np.sum(pred_viol_idx[t])
                                         for t in range(t2t.shape[0])]) / (t2t > 0).sum() * 100.0
+        precedence_magnitude = np.mean([np.dot(np.array([s[p] + t2t[t][p] - s[t]
+                                                         for p in range(t2t.shape[1]) if t2t[t][p] > 0]),
+                                               pred_viol_idx[t]) / len(pred_viol_idx[t]) if len(pred_viol_idx[t]) > 0 else 0
+                                        for t in range(t2t.shape[0])])
 
         # Resource consumption constraints
-        resource_violations = np.sum([
-            np.sum((np.array([
-                r2t[r][t] + np.dot((np.array([(s[t] - s[tp])*(s[tp] + dur[tp] - s[t])
-                                     for tp in range(r2t.shape[1]) if tp != t and r2t[r][tp] > 0]) > 0).astype(int).T,
-                                   np.array([r2t[r][tp] for tp in range(r2t.shape[1]) if tp != t and r2t[r][tp] > 0]))
-                for t in range(r2t.shape[1]) if r2t[r][t] > 0
-             ]) > rc[r]).astype(int))
-            for r in range(r2t.shape[0])]) / (r2t > 0).sum() * 100.0
+        resource_viol_idx = [(np.array([r2t[r][t] +
+                                        np.dot((np.array([(s[t] - s[tp])*(s[tp] + dur[tp] - s[t])
+                                                            for tp in range(r2t.shape[1]) if tp != t and r2t[r][tp] > 0]) > 0).astype(int).T,
+                                                np.array([r2t[r][tp] for tp in range(r2t.shape[1]) if tp != t and r2t[r][tp] > 0]))
+                                        for t in range(r2t.shape[1]) if r2t[r][t] > 0]) > rc[r]).astype(int)
+                             for r in range(r2t.shape[0])]
+        resource_violations = np.sum([np.sum(resource_viol_idx[r]) for r in range(r2t.shape[0])]) / (r2t > 0).sum() * 100.0
+        resource_magnitude = np.mean([np.dot(np.array([r2t[r][t] - rc[r] +
+                                                       np.dot((np.array([(s[t] - s[tp])*(s[tp] + dur[tp] - s[t])
+                                                                         for tp in range(r2t.shape[1]) if tp != t and r2t[r][tp] > 0]) > 0).astype(int).T,
+                                                               np.array([r2t[r][tp] for tp in range(r2t.shape[1]) if tp != t and r2t[r][tp] > 0]))
+                                                       for t in range(r2t.shape[1]) if r2t[r][t] > 0]),
+                                             resource_viol_idx[r]) / len(resource_viol_idx[r]) if len(resource_viol_idx[r]) > 0 else 0
+                                      for r in range(r2t.shape[0])])
 
         # Makespan cost
         makespan = np.max(s + dur) / ref_makespan #(nb_tasks * nb_res)
 
-        total_violations['all_positive'].append(all_positive_violations)
-        total_violations['precedence'].append(precedence_violations)
-        total_violations['resource'].append(resource_violations)
+        total_violations['all_positive_per'].append(all_positive_violations)
+        total_violations['all_positive_mag'].append(all_positive_magnitude)
+        total_violations['precedence_per'].append(precedence_violations)
+        total_violations['precedence_mag'].append(precedence_magnitude)
+        total_violations['resource_per'].append(resource_violations)
+        total_violations['resource_mag'].append(resource_magnitude)
         total_violations['makespan'].append(makespan)
         
     for k in total_violations:
@@ -70,16 +102,19 @@ def check_solution(data):
     return total_violations
 
 
-def train(train_loader, model, optimizer):
+def train(train_loader, model, optimizer, device):
     model.train()
     NUM_EPOCHS = 1000
     step = 0
     for epoch in tqdm(range(NUM_EPOCHS)):
         epoch_violations = {
-            'all_positive': [],
-            'precedence': [],
+            'all_positive_per': [],
+            'all_positive_mag': [],
+            'precedence_per': [],
+            'precedence_mag': [],
+            'resource_per': [],
+            'resource_mag': [],
             'makespan': [],
-            'resource': [],
         }
         COMPUTE_METRICS = True if epoch % 100 == 0 else False
 
@@ -103,16 +138,15 @@ def train(train_loader, model, optimizer):
             out = model(data)
             data.out = out
             # TODO: is there a cleaner way to do this?
-            # data.__slices__['out'] = data.__slices__['x']
-            # data.__cat_dims__['out'] = data.__cat_dims__['x']
-            # data.__cumsum__['out'] = data.__cumsum__['x']
+            data._slice_dict['out'] = data._slice_dict['x']
+            data._inc_dict['out'] = data._inc_dict['x']
     #         data.cpu()
             
             if COMPUTE_DEBUG:
                 for hook in hooks:
                     hook.remove()
 
-            loss = F.mse_loss(data.out, data.solution_starts)
+            loss = compute_loss(data=data, device=device)
 
             if COMPUTE_DEBUG:
                 if loss.grad_fn is None:
@@ -139,9 +173,12 @@ def train(train_loader, model, optimizer):
         if COMPUTE_METRICS:
             for k, v in epoch_violations.items():
                 epoch_violations[k] = np.mean(epoch_violations[k])
-            writer.add_scalar('all_positive', epoch_violations['all_positive'], step)
-            writer.add_scalar('precedence', epoch_violations['precedence'], step)
-            writer.add_scalar('resource', epoch_violations['resource'], step)
+            writer.add_scalar('all_positive_per', epoch_violations['all_positive_per'], step)
+            writer.add_scalar('all_positive_mag', epoch_violations['all_positive_mag'], step)
+            writer.add_scalar('precedence_per', epoch_violations['precedence_per'], step)
+            writer.add_scalar('precedence_mag', epoch_violations['precedence_mag'], step)
+            writer.add_scalar('resource_per', epoch_violations['resource_per'], step)
+            writer.add_scalar('resource_mag', epoch_violations['resource_mag'], step)
             writer.add_scalar('makespan', epoch_violations['makespan'], step)
 
             torch.save(model.state_dict(), f"model_{epoch}.tch")
@@ -160,7 +197,8 @@ if __name__ == "__main__":
                 solution_file=os.path.join(solutions_dir, 'postpro_benchmark_merged_single_modes.json'))
     
     data_list = torch.load('data_list.tch')
-    train_loader = DataLoader(data_list, batch_size=32, shuffle=True)
+    train_list = random.sample(range(len(data_list)), int(0.8 * len(data_list)))
+    train_loader = DataLoader([data_list[d] for d in train_list], batch_size=32, shuffle=True)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     Net = ResTransformer
     # Net = ResGINE
@@ -172,4 +210,5 @@ if __name__ == "__main__":
     print(f"Run ID: {run_id}")
     writer = SummaryWriter(f's3://iooda-gnn4rcpsp-bucket/tensorboard_logs/{run_id}')
     
-    train(train_loader=train_loader, model=model, optimizer=optimizer)
+    torch.save(train_list, './train_list.tch')
+    train(train_loader=train_loader, model=model, optimizer=optimizer, device=device)
