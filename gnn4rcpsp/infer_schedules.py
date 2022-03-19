5fddfd6f8e2a6128cc4188b1af8e28f5a292dd31
Florent Teichteil-Koenigsbuch <florent.teichteil-koenigsbuch@airbus.com>
2022-03-19 18:32:03 +0100

    Make inferred schedule feasible with nlopt or ortools

--- gnn4rcpsp/infer_schedules.py
+++ gnn4rcpsp/infer_schedules.py
@@ -5,9 +5,172 @@ from torch.utils.tensorboard import SummaryWriter
 from torch_geometric.data import DataLoader
 from tqdm import tqdm
 
+import nlopt
+from ortools.sat.python import cp_model
+
 from learn_schedules import check_solution, compute_loss
 
 from models import ResTransformer, ResGINE
+
+
+def make_feasible_nlopt(data):
+    
+    def objective(xorig, x, dur, grad):
+        v = np.array([x[i] - xorig[i] for i in range(len(x))])
+        for j in range(len(grad)):
+            grad[j] = (1. / float(len(x))) * 2. * v[j]
+        return (1. / float(len(x))) * np.dot(v, v.T)
+    
+    def all_positive_contraint(i, x, grad):
+        for j in range(len(grad)):
+            grad[j] = 0.
+        grad[i] = -1.
+        return -x[i]
+    
+    def precedence_constraint(t, p, t2t, x, grad):
+        for j in range(len(grad)):
+            grad[j] = 0.
+        grad[p] = 1.
+        grad[t] = -1.
+        return x[p] + t2t[t][p] - x[t]
+    
+    def resource_constraint(r, t, r2t, dur, x, grad):
+        # Wrong derivative on purpose!
+        # (assume ramp function for constraintderivative but heaviside for constraint value)
+        for j in range(len(grad)):
+            grad[j] = 0.
+        for tp in range(r2t.shape[1]):
+            if tp != t and r2t[r][tp] > 0:
+                grad[tp] = r2t[r][tp] * (2*x[t] - 2*x[tp] - dur[tp]) if (x[t] - x[tp])*(x[tp] + dur[tp] - x[t]) > 0 else 0.
+        grad[t] = np.dot((np.array([(x[t] - x[tp])*(x[tp] + dur[tp] - x[t])
+                                                     for tp in range(r2t.shape[1]) if tp != t and r2t[r][tp] > 0]) > 0).astype(int).T,
+                                          np.array([(2*x[tp] - 2*x[t] + dur[tp]) * r2t[r][tp] for tp in range(r2t.shape[1]) if tp != t and r2t[r][tp] > 0]))
+        return r2t[r][t] - rc[r] + np.dot((np.array([(x[t] - x[tp])*(x[tp] + dur[tp] - x[t])
+                                                     for tp in range(r2t.shape[1]) if tp != t and r2t[r][tp] > 0]) > 0).astype(int).T,
+                                          np.array([r2t[r][tp] for tp in range(r2t.shape[1]) if tp != t and r2t[r][tp] > 0]))
+    
+    data_batch = data
+    # Iterate over batch elements for simplicity
+    # TODO: vectorize?
+    data_list = data_batch.to_data_list()
+    for data in data_list:
+        
+        t2t, dur, r2t, rc, con, ref_makespan = (
+            data.t2t.view(len(data.dur), -1).data.cpu().detach().numpy(),
+            data.dur.data.cpu().detach().numpy(),
+            data.r2t.view(len(data.rc), len(data.dur)).data.cpu().detach().numpy(),
+            data.rc.data.cpu().detach().numpy(),
+            data.con.view(*data.con_shape).data.cpu().detach().numpy(),
+            data.reference_makespan
+        )
+        
+        xorig = data.out[len(rc):,0].cpu().detach().numpy()
+        
+        nb_tasks = len(dur)
+        # opt = nlopt.opt(nlopt.LD_MMA, nb_tasks)
+        opt = nlopt.opt(nlopt.LD_MMA, nb_tasks)
+        # don't use lower bounds since they assume xorig to be feasible
+        # => prefer all_positive_constraint because inequality constraints don't assume this
+        # opt.set_lower_bounds(np.zeros(shape=(nb_tasks,), dtype=np.float32))
+        
+        opt.set_min_objective(lambda x, grad: objective(xorig, x, dur, grad))
+
+        for i in range(nb_tasks):
+            opt.add_inequality_constraint(lambda x, grad: all_positive_contraint(i, x, grad), 1e-8)
+        
+        for t in range(t2t.shape[0]):
+            for p in range(t2t.shape[1]):
+                if t2t[t][p] > 0:
+                    opt.add_inequality_constraint(lambda x, grad: precedence_constraint(t, p, t2t, x, grad), 1e-8)
+
+        for r in range(r2t.shape[0]):
+            for t in range(r2t.shape[1]):
+                if r2t[r][t] > 0:
+                    opt.add_inequality_constraint(lambda x, grad: resource_constraint(r, t, r2t, dur, x, grad), 1e-8)
+        
+        opt.set_xtol_rel(1e-4)
+        xdest = opt.optimize(xorig)
+        
+        feasible = True
+        grad = np.zeros(shape=(nb_tasks,))
+        
+        for i in range(nb_tasks):
+            feasible = feasible and (all_positive_contraint(i, xdest, grad) <= 1e-8)
+        
+        for t in range(t2t.shape[0]):
+            for p in range(t2t.shape[1]):
+                if t2t[t][p] > 0:
+                    feasible = feasible and (precedence_constraint(t, p, t2t, xdest, grad) <= 1e-8)
+
+        for r in range(r2t.shape[0]):
+            for t in range(r2t.shape[1]):
+                if r2t[r][t] > 0:
+                    feasible = feasible and (resource_constraint(r, t, r2t, dur, xdest, grad) <= 1e-8)
+        
+        print("result code = ", opt.last_optimize_result())
+        print("makespan: {}".format(np.max(xdest + dur) / ref_makespan))
+        print("feasible: ", " YES" if feasible else " NO")
+
+
+def make_feasible_cpsat(data):
+    data_batch = data
+    # Iterate over batch elements for simplicity
+    # TODO: vectorize?
+    data_list = data_batch.to_data_list()
+    for data in data_list:
+        
+        t2t, dur, r2t, rc, con, ref_makespan = (
+            data.t2t.view(len(data.dur), -1).data.cpu().detach().numpy(),
+            data.dur.data.cpu().detach().numpy(),
+            data.r2t.view(len(data.rc), len(data.dur)).data.cpu().detach().numpy(),
+            data.rc.data.cpu().detach().numpy(),
+            data.con.view(*data.con_shape).data.cpu().detach().numpy(),
+            data.reference_makespan
+        )
+        xorig = np.around(data.out[len(rc):,0].cpu().detach().numpy(), decimals=0).astype(int)
+        
+        model = cp_model.CpModel()
+        horizon = int(max(xorig + dur) * 1.2)
+        starts = [model.NewIntVar(0, horizon - 1, 'start_task[{}]'.format(i)) for i in range(len(dur))]
+        ends = [model.NewIntVar(0, horizon - 1, 'start_task[{}]'.format(i)) for i in range(len(dur))]
+        durs = [model.NewConstant(int(dur[i])) for i in range(len(dur))]
+        intervals = [model.NewIntervalVar(starts[i], durs[i], ends[i], 'interval_task[{}]'.format(i)) for i in range(len(dur))]
+        makespan = model.NewIntVar(0, horizon - 1, 'makespan')
+        
+        # for t in range(t2t.shape[0]):
+        #     model.Add(starts[t] + durs[t] == ends[t])
+        
+        for t in range(t2t.shape[0]):
+            for p in range(t2t.shape[1]):
+                if t2t[t][p] > 0:
+                    model.Add(ends[p] <= starts[t])
+                    # model.AddNoOverlap([intervals[t], intervals[p]])  # redundant
+        
+        for r in range(r2t.shape[0]):
+            model.AddCumulative([intervals[t] for t in range(len(dur)) if r2t[r][t] > 0],
+                                [int(r2t[r][t]) for t in range(len(dur)) if r2t[r][t] > 0],
+                                int(rc[r]))
+            
+        model.AddMaxEquality(makespan, ends)
+        model.Add(makespan <= horizon)
+        
+        solver = cp_model.CpSolver()
+        model._CpModel__model.solution_hint.vars.extend(list(range(len(dur))))
+        model._CpModel__model.solution_hint.values.extend(xorig.tolist())
+        
+        status = solver.Solve(model)
+        if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
+            print('Solution found with relative makespan = {}, {}'.format(
+                solver.Value(makespan) / max(xorig + dur),
+                solver.Value(makespan) / max(ref_makespan)))
+        elif status == cp_model.INFEASIBLE:
+            print('Infeasible problem.')
+        elif status == cp_model.MODEL_INVALID:
+            print('Invalid model.')
+        elif status == cp_model.UNKNOWN:
+            print('Unknown status.')
+        else:
+            print('No solution found.')
 
 
 def test(test_loader, model, device, writer):
@@ -47,6 +210,8 @@ def test(test_loader, model, device, writer):
         writer.add_scalar('resource_per', batch_violations['resource_per'], batch_idx)
         writer.add_scalar('resource_mag', batch_violations['resource_mag'], batch_idx)
         writer.add_scalar('makespan', batch_violations['makespan'], batch_idx)
+        
+        make_feasible_cpsat(data)
     
         writer.flush()
 
