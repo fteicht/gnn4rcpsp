40e53ef6b3cc5190e069af1f270cfb6c6c8d5fc4
Florent Teichteil-Koenigsbuch <florent.teichteil@gmail.com>
2022-05-31 17:08:35 +0200

    Debugged graph and executor

--- gnn4rcpsp/executor.py
+++ gnn4rcpsp/executor.py
@@ -5,6 +5,7 @@ from typing import Dict, List, Set, Tuple
 from gym import make
 import torch
 import numpy as np
+from time import perf_counter
 from collections import defaultdict
 from ortools.sat.python import cp_model
 from models import ResTransformer
@@ -18,6 +19,9 @@ from skdecide.discrete_optimization.rcpsp.rcpsp_model import (
     create_poisson_laws,
 )
 from graph import Graph
+
+import networkx as nx
+import matplotlib.pyplot as plt
 
 CPSAT_TIME_LIMIT = 60  # 1 mn
 
@@ -69,41 +73,50 @@ class SchedulingExecutor:
         self._current_time = 0
         return self._executed_schedule, self._current_time
 
-    def progress(self, next_tasks: Set[int]) -> Tuple[int, RCPSPSolution, bool]:
-        """Takes the next task to execute and returns the new current time and updated executed schedule
+    def progress(
+        self, next_tasks: Set[int], next_start: int
+    ) -> Tuple[int, RCPSPSolution, bool]:
+        """Takes the next task to execute and their starting date and returns the new current time and updated executed schedule
         and a boolean indicating whether all the tasks have been scheduled or not"""
 
-        # Add next_task to the executed schedule
-        self._executed_schedule.rcpsp_schedule.update(
-            {
-                next_task: {
-                    "start_time": self._current_time,
-                    "end_time": self._current_time
-                    + max(
-                        mode["duration"]
-                        for mode in self._simulated_rcpsp.mode_details[
-                            next_task
-                        ].values()
-                    ),
-                }
-                for next_task in next_tasks
-            }
+        # Compute the next event, i.e. smallest task ending date
+        self._current_time = min(
+            [self._current_time + 1]  # Ensure to progress at least by 1 time unit
+            + [
+                sched["end_time"]
+                for sched in self._executed_schedule.rcpsp_schedule.values()
+                if sched["end_time"] > self._current_time
+            ]
         )
 
-        # Compute the next event, i.e. smallest task starting or ending date
-        event_times = set()
-        for dates in self._executed_schedule.rcpsp_schedule.values():
-            if dates["end_time"] > self._current_time:
-                event_times.add(dates["end_time"])
-        self._current_time = min(event_times) if len(event_times) > 0 else current_time
+        # Add next_task to the executed schedule if it is scheduled to start before running tasks have ended
+        if next_start <= self._current_time:
+            self._executed_schedule.rcpsp_schedule.update(
+                {
+                    next_task: {
+                        "start_time": next_start,
+                        "end_time": next_start
+                        + max(
+                            mode["duration"]
+                            for mode in self._simulated_rcpsp.mode_details[
+                                next_task
+                            ].values()
+                        ),
+                    }
+                    for next_task in next_tasks
+                }
+            )
+            self._current_time = min(
+                [self._current_time + 1]  # Ensure to progress at least by 1 time unit
+                + [
+                    sched["end_time"]
+                    for task, sched in self._executed_schedule.rcpsp_schedule.items()
+                    if task in next_tasks and sched["end_time"] > self._current_time
+                ]
+            )
 
-        # Update the mode details of the ended tasks
-        for task in self._executed_schedule.problem.mode_details.keys():
-            if (
-                task in self._executed_schedule.rcpsp_schedule
-                and self._executed_schedule.rcpsp_schedule[task]["end_time"]
-                <= self._current_time
-            ):
+            # Update the mode details of the started tasks
+            for task in next_tasks:
                 self._executed_schedule.problem.mode_details[
                     task
                 ] = self._simulated_rcpsp.mode_details[task]
@@ -118,9 +131,9 @@ class SchedulingExecutor:
 
     def next_tasks(
         self, executed_schedule: RCPSPSolution, current_time: int
-    ) -> Tuple[int, RCPSPSolution]:
+    ) -> Tuple[Set[int], int, int, RCPSPSolution]:
         """Takes a schedule which has been executed (i.e. list of committed start dates for some tasks),
-        the current time, and returns the next task's starting date and the full updated schedule"""
+        the current time, and returns the next tasks to start, their starting date, the expected makespan and the full updated schedule"""
 
         executed_rcpsp = executed_schedule.problem
 
@@ -134,20 +147,14 @@ class SchedulingExecutor:
             running_tasks = set(
                 task
                 for task, sched in executed_schedule.rcpsp_schedule.items()
-                if sched["end_time"] > current_time
+                if sched["start_time"] <= current_time
+                and sched["end_time"] > current_time
             )
             front_tasks = set()
             for task in executed_schedule.rcpsp_schedule:
                 front_tasks = front_tasks | set(executed_rcpsp.successors[task])
             front_tasks = front_tasks - set(executed_schedule.rcpsp_schedule.keys())
-            print("front tasks: {}".format(list(front_tasks)))
             front_tasks = front_tasks | running_tasks
-            print("running tasks: {}".format(list(running_tasks)))
-            print(
-                "executed tasks: {}".format(
-                    list(set(executed_schedule.rcpsp_schedule.keys()) - running_tasks)
-                )
-            )
 
         initial_task = -1
         initial_task_mode = {
@@ -204,12 +211,7 @@ class SchedulingExecutor:
             non_renewable_resources=executed_rcpsp.non_renewable_resources,
             mode_details=mode_details,
             successors=successors,
-            horizon=executed_rcpsp.horizon
-            - max(
-                dates["end_time"] for dates in executed_schedule.rcpsp_schedule.values()
-            )
-            if len(executed_schedule.rcpsp_schedule) > 0
-            else 0,
+            horizon=executed_rcpsp.horizon - current_time,
             horizon_multiplier=executed_rcpsp.horizon_multiplier,
             name_task=name_task,
         )
@@ -225,6 +227,9 @@ class SchedulingExecutor:
             uniform_law=True,
         )
 
+        starts_hint = {-1: 0}
+        starts_hint.update({task: 0 for task in running_tasks})
+
         # Sample RCPSPs and choose the best next task to start on average
         scenarios = defaultdict(lambda: [])
         for _ in range(self._samples):
@@ -232,7 +237,9 @@ class SchedulingExecutor:
                 MethodRobustification(MethodBaseRobustification.SAMPLE)
             )
 
-            status, makespan, feasible_solution = self.compute_schedule(sampled_rcpsp)
+            status, makespan, feasible_solution = self.compute_schedule(
+                sampled_rcpsp, starts_hint=starts_hint
+            )
 
             if status == cp_model.INFEASIBLE:
                 continue
@@ -260,18 +267,18 @@ class SchedulingExecutor:
 
         # Select the best tasks to start in term of average makespan
         best_tasks = None
+        best_next_start = None
         best_makespan = float("inf")
         for tasks, makespan_and_date in scenarios.items():
             if makespan_and_date[0] < best_makespan:
                 best_makespan = makespan_and_date[0]
+                best_next_start = makespan_and_date[1]
                 best_tasks = tasks
 
         # Compute the median RCPSP schedule with best tasks forced to start first
         # (the average or worst case ones appear to be barely feasible in practice)
         # worst_case_rcpsp = best_uncertain_rcpsp.create_rcpsp_model(MethodRobustification(MethodBaseRobustification.WORST_CASE))
-        starts_hint = {-1: 0}
-        starts_hint.update({task: 0 for task in running_tasks})
-        starts_hint.update({task: makespan_and_date[1] for task in best_tasks})
+        starts_hint.update({task: best_next_start for task in best_tasks})
         status, makespan, feasible_solution = self.compute_schedule(rcpsp, starts_hint)
         if status == cp_model.INFEASIBLE:
             raise RuntimeError("Infeasible schedule")
@@ -281,6 +288,7 @@ class SchedulingExecutor:
         # Return the best next tasks and the median RCPSP's makespan and schedule
         return (
             best_tasks - running_tasks,
+            best_next_start + current_time,
             makespan,
             RCPSPSolution(
                 problem=executed_rcpsp,
@@ -323,71 +331,78 @@ class SchedulingExecutor:
             data.out[len(rc) :, 0].cpu().detach().numpy(), decimals=0
         ).astype(int)
 
-        model = cp_model.CpModel()
-        horizon = int(max(xorig + dur) * 1.2)
-        starts = [
-            model.NewIntVar(0, horizon - 1, "start_task[{}]".format(i))
-            for i in range(len(dur))
-        ]
-        ends = [
-            model.NewIntVar(0, horizon - 1, "start_task[{}]".format(i))
-            for i in range(len(dur))
-        ]
-        durs = [model.NewConstant(int(dur[i])) for i in range(len(dur))]
-        intervals = [
-            model.NewIntervalVar(
-                starts[i], durs[i], ends[i], "interval_task[{}]".format(i)
+        horizon_start = int(max(xorig + dur))
+        current_horizon = horizon_start
+        curt = perf_counter()
+
+        while current_horizon <= 2 * horizon_start:
+
+            model = cp_model.CpModel()
+            horizon = current_horizon
+            starts = [
+                model.NewIntVar(0, horizon - 1, "start_task[{}]".format(i))
+                for i in range(len(dur))
+            ]
+            ends = [
+                model.NewIntVar(0, horizon - 1, "start_task[{}]".format(i))
+                for i in range(len(dur))
+            ]
+            durs = [model.NewConstant(int(dur[i])) for i in range(len(dur))]
+            intervals = [
+                model.NewIntervalVar(
+                    starts[i], durs[i], ends[i], "interval_task[{}]".format(i)
+                )
+                for i in range(len(dur))
+            ]
+            makespan = model.NewIntVar(0, horizon - 1, "makespan")
+
+            for t in range(t2t.shape[0]):
+                for p in range(t2t.shape[1]):
+                    if t2t[t][p] > 0:
+                        model.Add(ends[p] <= starts[t])
+                        # model.AddNoOverlap([intervals[t], intervals[p]])  # redundant
+
+            for r in range(r2t.shape[0]):
+                model.AddCumulative(
+                    [intervals[t] for t in range(len(dur)) if r2t[r][t] > 0],
+                    [int(r2t[r][t]) for t in range(len(dur)) if r2t[r][t] > 0],
+                    int(rc[r]),
+                )
+
+            model.AddMaxEquality(makespan, ends)
+
+            if starts_hint is not None:
+                constrained_starts = {
+                    i: model.NewConstant(starts_hint[t])
+                    for i, t in enumerate(rcpsp.successors)
+                    if t in starts_hint
+                }
+                for task_idx, start_constant in constrained_starts.items():
+                    model.Add(starts[task_idx] == start_constant)
+
+            # Search for a feasible solution
+            solver = cp_model.CpSolver()
+            solver.parameters.max_time_in_seconds = max(
+                0, CPSAT_TIME_LIMIT - int(perf_counter() - curt)
             )
-            for i in range(len(dur))
-        ]
-        makespan = model.NewIntVar(0, horizon - 1, "makespan")
+            xorig_list = xorig.tolist()
+            for i, x in enumerate(xorig_list):
+                model.AddHint(starts[i], x)
+            # model._CpModel__model.solution_hint.vars.extend(list(range(len(dur))))
+            # model._CpModel__model.solution_hint.values.extend(xorig.tolist())
+            status = solver.Solve(model)
 
-        for t in range(t2t.shape[0]):
-            for p in range(t2t.shape[1]):
-                if t2t[t][p] > 0:
-                    model.Add(ends[p] <= starts[t])
-                    # model.AddNoOverlap([intervals[t], intervals[p]])  # redundant
+            if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
+                solution = [solver.Value(s) for s in starts]
+                return (
+                    status,
+                    solver.Value(makespan),
+                    {t: solution[i] for i, t in enumerate(rcpsp.successors)},
+                )
+            else:
+                current_horizon = int(1.05 * current_horizon)
 
-        for r in range(r2t.shape[0]):
-            model.AddCumulative(
-                [intervals[t] for t in range(len(dur)) if r2t[r][t] > 0],
-                [int(r2t[r][t]) for t in range(len(dur)) if r2t[r][t] > 0],
-                int(rc[r]),
-            )
-
-        model.AddMaxEquality(makespan, ends)
-
-        if starts_hint is not None:
-            constrained_starts = {
-                i: model.NewConstant(starts_hint[t])
-                for i, t in enumerate(rcpsp.successors)
-                if t in starts_hint
-            }
-            for task_idx, start_constant in constrained_starts.items():
-                model.Add(starts[task_idx] == start_constant)
-
-        # Search for a feasible solution
-        solver = cp_model.CpSolver()
-        solver.parameters.max_time_in_seconds = CPSAT_TIME_LIMIT
-        xorig_list = xorig.tolist()
-        for i, x in enumerate(xorig_list):
-            model.AddHint(starts[i], x)
-        # model._CpModel__model.solution_hint.vars.extend(list(range(len(dur))))
-        # model._CpModel__model.solution_hint.values.extend(xorig.tolist())
-        status = solver.Solve(model)
-
-        if status == cp_model.INFEASIBLE:
-            foo = 0
-
-        if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
-            solution = [solver.Value(s) for s in starts]
-            return (
-                status,
-                solver.Value(makespan),
-                {t: solution[i] for i, t in enumerate(rcpsp.successors)},
-            )
-        else:
-            return (status, float("inf"), {})
+        return (status, float("inf"), {})
 
 
 if __name__ == "__main__":
@@ -407,10 +422,18 @@ if __name__ == "__main__":
     stop = False
 
     while not stop:
-        next_tasks, expected_makespan, expected_schedule = executor.next_tasks(
-            executed_schedule, current_time
+        (
+            next_tasks,
+            next_start,
+            expected_makespan,
+            expected_schedule,
+        ) = executor.next_tasks(executed_schedule, current_time)
+        print("Best tasks to start at time {}: {}".format(next_start, list(next_tasks)))
+        current_time, executed_schedule, stop = executor.progress(
+            next_tasks, next_start
         )
         print(
-            "Best tasks to start at time {}: {}".format(current_time, list(next_tasks))
+            "Executed schedule: {}\nExpected schedule: {}\nCurrent time: {}".format(
+                executed_schedule, expected_schedule, current_time
+            )
         )
-        current_time, executed_schedule, stop = executor.progress(next_tasks)
