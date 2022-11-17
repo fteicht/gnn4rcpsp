5992ff1855a581363e5aaf1403f6db5d8453a0e9
Florent Teichteil-Koenigsbuch <florent.teichteil-koenigsbuch@airbus.com>
2022-11-17 14:41:42 +0100

    bash multiprocess hindsight experimentations

--- gnn4rcpsp/execute_schedules.py
+++ gnn4rcpsp/execute_schedules.py
@@ -1,4 +1,5 @@
 import json
+import sys
 import os
 import warnings
 from collections import defaultdict
@@ -39,8 +40,8 @@ ExecutionModeNames = {
     ExecutionMode.HINDSIGHT_DBP: "HINDSIGHT_DBP",
 }
 
-NUM_HINDSIGHT_SAMPLES = 10
-NUM_INSTANCE_SCENARIOS = 2
+NUM_HINDSIGHT_SAMPLES = 30
+NUM_INSTANCE_SCENARIOS = 10
 PARALLEL = False
 
 
@@ -177,11 +178,11 @@ def execute_schedule(
                         for t in executed_schedule.rcpsp_schedule
                     }
                     print("method ", setup_name, " : ", current_time)
-                except:
+                except Exception as e:
                     makespans[f"Scenario {scn}"][setup_name]["executed"] = "Fail"
                     makespans[f"Scenario {scn}"][setup_name]["timing"] = "Fail"
                     makespans[f"Scenario {scn}"][setup_name]["schedule"] = "Fail"
-                    # print(e)
+                    print(e)
                     continue
 
         if PARALLEL:
@@ -259,45 +260,49 @@ if __name__ == "__main__":
     data_list = torch.load("../torch_data/data_list.tch")
     train_list = torch.load("../torch_data/train_list.tch")
     test_list = list(set(range(len(data_list))) - set(train_list))
-    filtered_test_list = [
-        1460,
-        509,
-        459,
-        450,
-        514,
-        234,
-        237,
-        391,
-        285,
-        1720,
-        231,
-        69,
-        406,
-        399,
-        1728,
-        1948,
-        1949,
-        502,
-        461,
-        1469,
-        1425,
-        471,
-        464,
-        527,
-        2032,
-        1244,
-        1858,
-        19,
-        1112,
-        303,
-        1556,
-        242,
-        37,
-        66,
-        522,
-        473,
-        1555,
-    ]
+    nargs = len(sys.argv)
+    if nargs > 1:
+        filtered_test_list = [int(a) for i, a in enumerate(sys.argv) if i > 1]
+    else:
+        filtered_test_list = [
+            1460,
+            509,
+            459,
+            450,
+            514,
+            234,
+            237,
+            391,
+            285,
+            1720,
+            231,
+            69,
+            406,
+            399,
+            1728,
+            1948,
+            1949,
+            502,
+            461,
+            1469,
+            1425,
+            471,
+            464,
+            527,
+            2032,
+            1244,
+            1858,
+            19,
+            1112,
+            303,
+            1556,
+            242,
+            37,
+            66,
+            522,
+            473,
+            1555,
+        ]
     assert set(filtered_test_list).issubset(set(test_list))
     test_loader = DataLoader(
         [data_list[d] for d in filtered_test_list], batch_size=1, shuffle=False
@@ -317,7 +322,11 @@ if __name__ == "__main__":
     )
     run_id = timestamp = datetime.utcnow().strftime("%Y%m%d%H%M%S")
     print(f"Run ID: {run_id}")
-    result_file = f"../hindsight_vs_reactive_{run_id}.json"
+    result_file = (
+        f"../hindsight_vs_reactive_{run_id}.json"
+        if nargs == 1
+        else f"experiments/{sys.argv[1]}/hindsight_vs_reactive_{run_id}.json"
+    )
     result = test(
         test_loader=test_loader,
         test_list=filtered_test_list,
